c7e1067e46e4198cd4daf63be82153364d2b4f81
maggiemthai <41025065+maggiemthai@users.noreply.github.com>
2020-11-26 13:59:42 -0700

    Added some main code; up to 6.d. Added header info.

--- main.cpp
+++ main.cpp
@@ -7,16 +7,32 @@
 #include <cmath>
 #include <math.h>
 #include "Lab9Structures.h"
-
+#include "MatrixIOFunctions.h"
 
 using namespace std;
 using namespace Eigen;
 
 
-
-
-
 int main()
 {
+  
+  //Reading data from txt file.
+MatrixXd PointCloud = ReadDataToMatrix("PointCloudData.txt");
+int r = PointCloud.rows();
+
+
+// Empty vector of Plane objects
+vector <Plane> planeobjects;
+
+// Matrix I with n x 1
+MatrixXd I;
+I.resize(r, 1);
+
+//Setting to Zero
+I.setZero();
+
+//Part d. of the lab, needs to loop 1-6 of Algorithm 2.
+
+
 
 }
