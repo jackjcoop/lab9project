49d11e29c08e608a108fefc85c9b480b6a75a758
jackjcoop <jackjcoop@gmail.com>
2020-11-26 11:26:48 -0700

    Initial commit

---
+++ Lab9Structures.h
@@ -0,0 +1,50 @@
+#ifndef LAB9STRUCTURES_H_INCLUDED
+#define LAB9STRUCTURES_H_INCLUDED
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Point
+{
+
+    double id; //Point ID
+    double xi; // x-coord
+    double yi; // y-coord
+    double zi; // z-coord
+
+};
+
+class Plane
+{
+
+//Private data members
+private:
+
+    double  nx_plane;
+    double  ny_plane;
+    double  nz_plane;
+    double  d_plane;
+
+//Public Constructors
+public:
+//Default Constructor (accepts no arguments)
+    Plane();
+
+//Constructor to receive values for private data members
+    Plane(double nx, double ny, double nz, double nd);
+
+//Set function, set values for all private data members
+    void set(double nx, double ny, double nz, double nd);
+
+//get function, retrieves values of private data members
+    Plane get(Plane P) const;
+
+//function to find distance between plane and the point
+    double pointdistance(Point& P) const;
+
+//mutator function that receives a matrix
+    Plane object(const Point& P) const;
+
+};
+#endif // LAB9STRUCTURES_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include "Eigen/Dense"
+#include <cmath>
+#include <math.h>
+#include "Lab9Structures.h"
+
+
+using namespace std;
+using namespace Eigen;
+
+
+//Default Constructor to Set to Zero
+Plane::Plane()
+    {
+        nx_plane = 0;
+        ny_plane = 0;
+        nz_plane = 0;
+        d_plane = 0;
+    }
+
+
+//Overload Constructor to receive values for private data members
+Plane::Plane(double nx, double ny, double nz, double nd)
+    {
+        nx_plane = nx;
+        ny_plane = ny;
+        nz_plane = nz;
+        d_plane = nd;
+
+        return;
+    }
+
+//Set function. Sets values for data members.
+void Plane::set(double nx, double ny, double nz, double nd)
+    {
+        nx_plane = nx;
+        ny_plane = ny;
+        nz_plane = nz;
+        d_plane = nd;
+
+        return;
+    }
+
+//Get function. Retrieves values. Help Jackson! How do I make this???
+Plane Plane::get(Plane P) const
+    {
+        Plane A = P;
+        return A;
+    }
+
+//Function to calculate distance between point and plane.
+double Plane::pointdistance(Point& P) const
+{
+    double pdistance;
+    struct Point arr = P;
+    Plane A;
+
+    double numerator = ((arr.xi * nx_plane) + (arr.yi * ny_plane) + (arr.zi * nz_plane) + d_plane);
+    double newnumerator = pow(numerator, 2);
+    double finalnumerator = sqrt(newnumerator);
+    double denominator = sqrt(pow(nx_plane,2) + pow(ny_plane,2) + pow(nz_plane,2));
+
+    pdistance = (finalnumerator/denominator);
+
+    return pdistance;
+
+}
+
+// Mutator function!!
+Plane Plane::object(const Point& P) const
+{
+
+
+
+}
+
+
+int main()
+{
+
+}
