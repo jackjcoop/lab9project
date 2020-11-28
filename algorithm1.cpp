f550403cb43ab3b325dc62b4c278642dfbc24dab
ElianDupre <eliandupre@gmail.com>
2020-11-27 19:15:53 -0700

    Update
    Still need implementation of "get" Plane function, and am having trouble concatenating the matrices (not sure how to do it in Eigen)

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -20,6 +20,7 @@ Plane fitPlane(vector<Point> pointsVec) { //argument is a vector of type point
     MatrixXd L = MatrixXd::Constant(numPoints, 3, 1);
 
     //put points x,y,z into each row of A
+    //NOTE: assumes xi,yi,zi are in the second, third, and fourth columns of the input vector
     for(int i = 0; i < numPoints - 1; i++) {
         for (int j = 1; j < 4; j++) {
             if (j == 1) {
@@ -75,11 +76,43 @@ Plane fitPlane(vector<Point> pointsVec) { //argument is a vector of type point
 
                 //i-th row of W is set to -(nxXi + nyYi + nzZi + d)
                 if (j == 0) { //this is a slightly silly if statement, but it only runs once per i increment so I win
-                    W(i,0) = -1*(result.get(result).nx_plane*pointsVec[i].xi + result(i,j+1)*pointsVec[i].yi + result(i,j+2)*pointsVec[i].zi + pointsVec[i].di); //this bit says "no match for call to '(Plane) (int&, int&)'
-                } //Need to figure out how to access the Plane 'result' member values
+                   // W(i,0) = -1*(result.get(nx_plane)*pointsVec[i].xi + result.get(ny_plane)*pointsVec[i].yi + result.get(nz_plane)*pointsVec[i].zi + pointsVec[i].id);
+                } //**CODE ABOVE COMMENTED OUT** Need to figure out how to access the Plane 'result' member values
 
             }
         }
+
+        //create unit vector H
+        MatrixXd H = MatrixXd(1,4);
+//            H(0,0) = result.get(nx_plane)/u;
+//            H(0,1) = result.get(ny_plane)/u;  // **NEED PLANE GET FUNCTION** (don't mean to be aggressive haha)
+//            H(0,2) = result.get(nz_plane)u;
+            H(0,3) = 0;
+
+        //create 1-by-1 matrix Hc
+        MatrixXd Hc = MatrixXd(1,1);
+            Hc(0,0) = (1-u);
+
+        //create new matrix N
+        MatrixXd N = MatrixXd();
+        N = A.transpose()*A;
+
+        //create new matrix U
+        MatrixXd U = MatrixXd();
+        U = A.transpose()*W;
+
+        //create new matrix Uc
+        MatrixXd Uc = MatrixXd();
+       // Uc << U << Hc;                HOW DO YOU CONCATENATE MATRICES???
+
+        //create new matrix K
+        MatrixXd K = MatrixXd();
+        //K << N << H.transpose() << H << 0;       HOW DO YOU CONCATENATE MATRICES???
+
+        deltaX = K.inverse()*Uc;
+
+        //result.Set(deltaX(0,1) + result.get(nx_plane), deltaX(0,2) + result.get(ny_plane), deltaX(0,3) + result.get(nz_plane), deltaX(0,4) + result.get(d_plane)); // **NEED PLANE GET FUNCTION** (don't mean to be aggressive haha)
+
     }
 
 
