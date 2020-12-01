c6f53175d2f475a632d2beda7a3b59ff8a863968
maggiemthai <41025065+maggiemthai@users.noreply.github.com>
2020-11-30 23:23:33 -0700

    Update Lab9Structures.cpp

--- Lab9Structures.cpp
+++ Lab9Structures.cpp
@@ -8,6 +8,8 @@
 #include <math.h>
 #include "Lab9Structures.h"
 
+using namespace std;
+using namespace Eigen;
 //Default Constructor to Set to Zero
 Plane::Plane()
     {
@@ -150,6 +152,87 @@ void Plane::algorithm1(Eigen::MatrixXd matrix)
 
 
 }
+
+vector<bool> Plane::RANSAC(MatrixXd matrix) { //argument is a vector of type point
+
+    //intializing intial values of 0 and the length of points numPoints
+    int t,c= 0;
+    int numPoints = matrix.rows();
+
+    //intializing the boolean vector of size numPoints from the inputted matrix
+    vector<bool> I;
+    I.resize(numPoints);
+
+    do
+    {
+        t += 1;
+        MatrixXd B(3,3);
+
+        for(int i = 0; i < numPoints; i++)
+        {
+            I[t] = false;
+        }
+
+
+        //populating matrix B with random
+        for(int i = 0; i < 3; i++)
+        {
+            double rand_row = rand()%B.rows();
+            for(int j = 0; j < 3; j++)
+            {
+               B(i,j) = matrix(rand_row,j);
+            }
+
+        }
+
+        //passing B into algorithm one
+        //algorithm_1(B);
+
+        c = 0;
+
+        for(int j = 0; j < numPoints; j++)
+        {
+           //intializing point object with coordiantes x,y,z
+           Point object;
+           object.x = matrix(j,1);
+           object.y = matrix(j,2);
+           object.z = matrix(j,3);
+
+           //computing distance of the object
+           double distance_length = distance(object);
+
+           if(distance_length < 0.01)
+           {
+            I[j] = true;
+            c++;
+           }
+        }
+    }
+    while (c < (numPoints / 2) && t < 50);
+
+    MatrixXd P(c,3);
+
+    for(int i = 0; i < numPoints; i++)
+    {
+        if(I[i] == true)
+        {
+            P(i,0) = matrix(i,0);
+            P(i,1) = matrix(i,1);
+            P(i,2) = matrix(i,2);
+        }
+    }
+    algorithm1(P);
+
+    return I;
+
+}
+
+
+
+
+
+
+
 
 
 
